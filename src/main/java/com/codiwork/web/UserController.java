eaec56648e9e994793b3df2d625cc49beefb9127
PC-HOME\basiang <plushand@gmail.com>
2017-07-08 22:04:54 +0900

    회원가입, 사용자 목록 기능 구현

---
+++ src/main/java/com/codiwork/web/UserController.java
@@ -0,0 +1,31 @@
+package com.codiwork.web;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
+	private List<User> users = new ArrayList<User>();
+	
+	@PostMapping("/create")
+	public String create(User user){
+		System.out.println("userId : " + user.getUserId());
+		System.out.println("password : " + user.getPassword());
+		System.out.println("name : " + user.getName());
+		System.out.println("email : " + user.getEmail());
+		System.out.println("user : " + user.toString());
+		users.add(user);
+		return "redirect:/list";
+	}
+	
+	@GetMapping("/list")
+	public String list(Model model){
+		model.addAttribute("users", users);
+		return "list";
+	}
+}

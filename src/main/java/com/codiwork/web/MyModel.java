8c19a1c789df08cdfeba238fbead207b9dd47286
PC-HOME\basiang <plushand@gmail.com>
2017-07-08 22:57:57 +0900

    test

---
+++ src/main/java/com/codiwork/web/MyModel.java
@@ -0,0 +1,19 @@
+package com.codiwork.web;
+
+public class MyModel {
+	private String name;
+
+	public MyModel(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}

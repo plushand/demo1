5a181e917692937abb57deea4d4048207d9e0171
PC-HOME\basiang <plushand@gmail.com>
2017-07-09 10:02:53 +0900

    로그인 기능 추가

--- src/main/java/com/codiwork/web/UserController.java
+++ src/main/java/com/codiwork/web/UserController.java
@@ -3,6 +3,8 @@ package com.codiwork.web;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -23,6 +25,27 @@ public class UserController {
 	@Autowired
 	private UserRepository userRepository;
 
+	@GetMapping("/loginForm")
+	public String loginForm(){
+		return "/user/login";
+	}
+	
+	@PostMapping("/login")
+	public String login(String userId, String password
+			,HttpSession session){
+		User user = userRepository.findByUserId(userId);
+		if (user == null) {
+			return "redirect:/users/loginForm";
+		}
+		if (!password.equals(user.getPassword())){
+			return "redirect:/users/loginForm";
+		}
+
+		session.setAttribute("user", user);
+		
+		return "redirect:/";
+	}
+	
 	@GetMapping("/form")
 	public String form(){
 		return "/user/form";

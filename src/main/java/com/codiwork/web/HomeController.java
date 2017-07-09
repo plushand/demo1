f050f786768d9bf6db1824f0d89f9170d47505c4
PC-HOME\basiang <plushand@gmail.com>
2017-07-09 12:10:33 +0900

    외부톰켓에 알림

--- src/main/java/com/codiwork/web/HomeController.java
+++ src/main/java/com/codiwork/web/HomeController.java
@@ -1,12 +1,20 @@
 package com.codiwork.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.codiwork.domain.QuestionRepository;
 
 @Controller
 public class HomeController {
+	@Autowired
+	private QuestionRepository questionRepository;
+	
 	@GetMapping("")
-	public String home(){
+	public String home(Model model){
+		model.addAttribute("questions", questionRepository.findAll());
 		return "index";
 	}
 }
